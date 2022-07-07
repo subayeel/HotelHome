73d0ce90bd26186be269b340e4ee795dc0aed01d
Abdullah Subayeel <abdullahsubayeel999@gmail.com>
2022-07-07 13:14:52 +0530

    Navbar styled-component

--- src/Components/styled/Avatar.styled.js
+++ src/Components/styled/Avatar.styled.js
@@ -4,15 +4,15 @@ export const StyledAvatar = styled.div`
 
 display: inline-flex;
 border-radius: 100%;
-height: 50px;
-width: 50px;
+height: 60px;
+width: 60px;
 margin: 10px;
 background-color: ${({theme})=> theme.colors.primaryColor};
 img{
   align-items: center;
       margin: auto;
       width:100%;
-      height: 100%;
+      
 }
 
 
